f06a2bf997183fd098cb3e1f38052619af16b3e2
yanhuan <huan.yan@tcredit.com>
2018-02-11 16:01:53 +0800

    初始化项目

---
+++ src/main/java/com/springboot/platform/Application.java
@@ -0,0 +1,42 @@
+package com.springboot.platform;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Processor;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+
+
+@SpringBootApplication
+@EnableBinding({Processor.class})
+public class Application {
+
+    @Autowired
+    @Qualifier("output")
+    MessageChannel output;
+
+
+//    @Autowired
+//    ProductProcessor productProcessor;
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+
+
+    @StreamListener(Processor.INPUT)
+    public void input1(Message<String> message) {
+        System.out.println("接收消息" + message.getPayload());
+    }
+
+
+}
+
+
+
+
---
+++ src/main/java/com/springboot/platform/ProductProcessor.java
@@ -0,0 +1,22 @@
+package com.springboot.platform;
+
+import org.springframework.cloud.stream.annotation.Input;
+import org.springframework.cloud.stream.annotation.Output;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.SubscribableChannel;
+
+public interface ProductProcessor {
+
+
+    // 自定义的input 和output
+    String INPUT_PRODUCT_ADD = "inputProductAdd";
+
+    String OUTPUT_PRODUCT_ADD = "outputProductAdd";
+
+    @Input(INPUT_PRODUCT_ADD)
+    SubscribableChannel inputProductAdd();  // 定义输入通道
+
+    @Output(OUTPUT_PRODUCT_ADD)
+    MessageChannel outputProductAdd(); // 定义输出message消息通道
+
+}
